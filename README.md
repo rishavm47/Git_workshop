4d19a3edd6577d284f2eb27e75ee9aa5bb5641aa
Rishav <rishavm47@gmail.com>
2018-10-08 18:40:03 +0530

    updated both helloword and readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Git_workshop
 Learning Git by Owasp
+Hey, now I pulled the request
--- hello_word.cpp
+++ hello_word.cpp
@@ -1,1 +1,8 @@
-adfhhadpfhpadodidh;asdlskdn;aosiddjfjfasodiod
+#include <iostream>
+
+using namespace std;
+int main()
+{
+cout<<"hello";
+return 0;
+}
